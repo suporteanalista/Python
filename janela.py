5902b6778a273ed612b4a6940edeab35bda93962
sac <diogo@suporteanalista.com>
2023-11-26 18:40:44 -0300

    Terminei Primeira parte interface de Login Python dia 26/11/2023

--- janela.py
+++ janela.py
@@ -15,6 +15,9 @@
 # janela.mainloop()
 import customtkinter
 
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
 janela = customtkinter.CTk()
 janela.geometry ("500x300")
 
@@ -29,6 +32,9 @@ email.pack(padx=10, pady=10)
 
 senha = customtkinter.CTkEntry(janela, placeholder_text="Sua senha", show="*")
 senha.pack(padx=10, pady=10)
+
+checkbox = customtkinter.CTkCheckBox(janela, text="Lembrar login")
+checkbox.pack(padx=10, pady=10)
 
 botao = customtkinter.CTkButton(janela, text="Login", command=clique)
 botao.pack(padx=10, pady=10)
